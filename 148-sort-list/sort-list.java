65081105800d543c410017402742990d67d1976d
Siddhesh Pokharkar <143425825+SiddheshPok@users.noreply.github.com>
2026-07-03 13:20:23 +0530

    Time: 14 ms (22.88%) | Memory: 62.6 MB (20.91%) - LeetSync

---
+++ 148-sort-list/sort-list.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public ListNode sortList(ListNode head) {
+
+        if (head == null || head.next == null) return head;
+
+        ArrayList<Integer> list = new ArrayList<>();
+        ListNode curr = head;
+
+        while (curr != null) {
+            list.add(curr.val);
+            curr = curr.next;
+        }
+
+        Collections.sort(list);
+
+        curr = head;
+        int i = 0;
+        while (curr != null) {
+            curr.val = list.get(i++);
+            curr = curr.next;
+        }
+        return head;
+    }
+}
