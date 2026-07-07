50e6fad9af8f5034d30215def1a8ff44cd040592
Siddhesh Pokharkar <143425825+SiddheshPok@users.noreply.github.com>
2026-07-07 20:55:32 +0530

    Time: 1 ms (83.46%) | Memory: 43 MB (18.00%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+
+        String[] arr = s.split(" ");
+
+        if (pattern.length() != arr.length) {
+            return false;
+        }
+
+        HashMap<Character, String> map = new HashMap<>();
+        HashMap<String, Character> reverse = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            char c = pattern.charAt(i);
+
+            if (!map.containsKey(c)) {
+                map.put(c, arr[i]);
+            }
+
+            if (!reverse.containsKey(arr[i])) {
+                reverse.put(arr[i], c);
+            }
+
+            if (!map.get(c).equals(arr[i]) || reverse.get(arr[i]) != c) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
