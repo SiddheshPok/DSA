cee6438bb4b6fcb7baf7b994381d3268952177ba
Siddhesh Pokharkar <143425825+SiddheshPok@users.noreply.github.com>
2026-07-07 10:15:45 +0530

    Time: 1 ms (99.85%) | Memory: 42.6 MB (67.71%) - LeetSync

--- 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ 4135-concatenate-non-zero-digits-and-multiply-by-sum-i/concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -1,19 +1,37 @@
 class Solution {
     public long sumAndMultiply(int n) {
-        int sum = 0;
-        String s = Integer.toString(n);
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < s.length(); i++)
+        // int sum = 0;
+        // String s = Integer.toString(n);
+        // StringBuilder sb = new StringBuilder();
+        // for(int i = 0; i < s.length(); i++)
+        // {
+        //     int nn = s.charAt(i) - '0';
+        //     if(nn != 0)
+        //     {
+        //         sum += nn;
+        //         sb.append(s.charAt(i));
+        //     }
+        // }
+        // if (sb.length() == 0) return 0;
+        // long result = Long.parseLong(sb.toString());
+        // return result * sum;
+        long sum = 0;
+        long ans = 0;
+        long num = 0;
+        while(n != 0)
         {
-            int nn = s.charAt(i) - '0';
-            if(nn != 0)
+            if(n % 10 != 0)
             {
-                sum += nn;
-                sb.append(s.charAt(i));
+                sum += n % 10;
+                num = (num * 10) + (n % 10);
             }
+            n = n / 10;
         }
-        if (sb.length() == 0) return 0;
-        long result = Long.parseLong(sb.toString());
-        return result * sum;
+        while(num != 0)
+        {
+            ans = (ans * 10)+ (num % 10);
+            num /= 10;
+        }
+        return sum*ans;
     }
 }
