6c35b6638890bbffa52439ebd80f8992624ce243
Siddhesh Pokharkar <143425825+SiddheshPok@users.noreply.github.com>
2026-07-26 11:16:55 +0530

    Time: 25 ms (46.71%) | Memory: 48.4 MB (93.96%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -17,5 +17,19 @@ class Solution {
             map.put(pref_sum,map.getOrDefault(pref_sum,0)+1);
         }
         return count;
+
+
+        // int n = nums.length;
+        // int count = 0;
+        // for(int i = 0; i < n; i++){
+        //     int sum = 0;
+        //     for(int j = i ; j < n; j++){
+        //         sum += nums[j];
+        //         if(sum == k){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
     }
 }
