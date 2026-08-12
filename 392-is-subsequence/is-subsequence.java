9d53bc8fbf4f2f1d93a755d99ff1f90c4860d22b
Siddhesh Pokharkar <143425825+SiddheshPok@users.noreply.github.com>
2026-08-12 19:41:12 +0530

    Time: 2 ms (73.73%) | Memory: 42.9 MB (26.16%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,22 +1,36 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int ns = s.length();
-        int nt = t.length();
-        int k = 0;
-        int count = 0;
+        // int ns = s.length();
+        // int nt = t.length();
+        // int k = 0;
+        // int count = 0;
 
-        for(int i = 0; i < nt; i++)
+        // for(int i = 0; i < nt; i++)
+        // {
+        //     char ch = t.charAt(i);
+        //     if(s.contains(String.valueOf(ch)))
+        //     {
+        //         if(ch == s.charAt(k))
+        //         {
+        //             k++;
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count == ns;
+
+        int k = 0;
+        for(int i = 0; i < t.length(); i++)
         {
-            char ch = t.charAt(i);
-            if(s.contains(String.valueOf(ch)))
+            if(k < s.length() && t.charAt(i) == s.charAt(k))
             {
-                if(ch == s.charAt(k))
-                {
-                    k++;
-                    count++;
-                }
+                k++;
+            }
+            if(k == s.length())
+            {
+                return true;
             }
         }
-        return count == ns;
+        return s.length() == k;
     }
 }
