dfe6160ce89e7272316f5325f82ecb08380552f3
Siddhesh Pokharkar <143425825+SiddheshPok@users.noreply.github.com>
2026-08-17 13:05:20 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (22.34%) - LeetSync

--- 3321-type-of-triangle/type-of-triangle.java
+++ 3321-type-of-triangle/type-of-triangle.java
@@ -1,38 +1,55 @@
 class Solution {
     public String triangleType(int[] nums) {
-        if(nums[0] + nums[1] > nums[2] && nums[0] + nums[2] > nums[1] && nums[1] + nums[2] > nums[0])
+        int a = nums[0];
+        int b = nums[1];
+        int c = nums[2];
+
+        if(a + b <= c || a + c <= b || b + c <= a)
         {
-            // HashMap<Integer,Integer> map = new HashMap<>();
-            // for(int i = 0; i < nums.length; i++)
-            // {
-            //     map.put(nums[i], map.getOrDefault(nums[i],0)+1);
-            // }
-            // int n = map.size();
-            if(nums[0] == nums[1] && nums[1] == nums[2])
-            {
-                return "equilateral";
-            }
-            else if(nums[0] != nums[1] && nums[1] != nums[2] && nums[0] != nums[2])
-            {
-                return "scalene";
-            }
-            else
-            {
-                return "isosceles";
-            }
-            // if(n == 1)
-            // {
-            //     return "equilateral";
-            // }
-            // else if(n == 3)
-            // {
-            //     return "scalene";
-            // }
-            // else
-            // {
-            //     return "isosceles";
-            // }
+            return "none";
         }
-        return "none";
+        if(a == b && b == c)
+        {
+            return "equilateral";
+        }
+        if(a != b && b != c && a != c)
+        {
+            return "scalene";
+        }
+        return "isosceles";
+        // if(nums[0] + nums[1] > nums[2] && nums[0] + nums[2] > nums[1] && nums[1] + nums[2] > nums[0])
+        // {
+        //     // HashMap<Integer,Integer> map = new HashMap<>();
+        //     // for(int i = 0; i < nums.length; i++)
+        //     // {
+        //     //     map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+        //     // }
+        //     // int n = map.size();
+        //     if(nums[0] == nums[1] && nums[1] == nums[2])
+        //     {
+        //         return "equilateral";
+        //     }
+        //     else if(nums[0] != nums[1] && nums[1] != nums[2] && nums[0] != nums[2])
+        //     {
+        //         return "scalene";
+        //     }
+        //     else
+        //     {
+        //         return "isosceles";
+        //     }
+        //     // if(n == 1)
+        //     // {
+        //     //     return "equilateral";
+        //     // }
+        //     // else if(n == 3)
+        //     // {
+        //     //     return "scalene";
+        //     // }
+        //     // else
+        //     // {
+        //     //     return "isosceles";
+        //     // }
+        // }
+        // return "none";
     }
 }
