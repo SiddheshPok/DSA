f4d23f50c1c4676fddbc33789a60fe63bbfb53d7
Siddhesh Pokharkar <143425825+SiddheshPok@users.noreply.github.com>
2026-08-17 12:57:25 +0530

    Time: 1 ms (57.10%) | Memory: 43.9 MB (22.34%) - LeetSync

--- 3321-type-of-triangle/type-of-triangle.java
+++ 3321-type-of-triangle/type-of-triangle.java
@@ -2,17 +2,17 @@ class Solution {
     public String triangleType(int[] nums) {
         if(nums[0] + nums[1] > nums[2] && nums[0] + nums[2] > nums[1] && nums[1] + nums[2] > nums[0])
         {
-            HashMap<Integer,Integer> map = new HashMap<>();
-            for(int i = 0; i < nums.length; i++)
-            {
-                map.put(nums[i], map.getOrDefault(nums[i],0)+1);
-            }
-            int n = map.size();
-            if(n == 1)
+            // HashMap<Integer,Integer> map = new HashMap<>();
+            // for(int i = 0; i < nums.length; i++)
+            // {
+            //     map.put(nums[i], map.getOrDefault(nums[i],0)+1);
+            // }
+            // int n = map.size();
+            if(nums[0] == nums[1] && nums[1] == nums[2])
             {
                 return "equilateral";
             }
-            else if(n == 3)
+            else if(nums[0] != nums[1] && nums[1] != nums[2] && nums[0] != nums[2])
             {
                 return "scalene";
             }
@@ -20,6 +20,18 @@ class Solution {
             {
                 return "isosceles";
             }
+            // if(n == 1)
+            // {
+            //     return "equilateral";
+            // }
+            // else if(n == 3)
+            // {
+            //     return "scalene";
+            // }
+            // else
+            // {
+            //     return "isosceles";
+            // }
         }
         return "none";
     }
