a262fed8012de5ddda40c70397791b7707daaf48
Siddhesh Pokharkar <143425825+SiddheshPok@users.noreply.github.com>
2026-08-23 19:59:33 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (57.51%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,20 +1,6 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) 
-    {
-        // List<List<Integer>> ans = new ArrayList<>();
-        // ans.add(new ArrayList<>());
-
-        // for(int num : nums) 
-        // {
-        //     int size = ans.size();
-        //     for(int i = 0; i < size; i++) 
-        //     {
-        //         List<Integer> list = new ArrayList<>(ans.get(i));
-        //         list.add(num);
-        //         ans.add(list);
-        //     }
-        // }
-        // return ans;
+        {
         List<List<Integer>> ans = new ArrayList<>();
         solve(nums, 0, new ArrayList<>(), ans);
         return ans;
