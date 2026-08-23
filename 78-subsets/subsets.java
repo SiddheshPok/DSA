0b4151984f5ea77829e90ca0db621d66b46de74e
Siddhesh Pokharkar <143425825+SiddheshPok@users.noreply.github.com>
2026-08-23 19:57:55 +0530

    Time: 1 ms (86.42%) | Memory: 44.3 MB (39.31%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,19 +1,34 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
+    public List<List<Integer>> subsets(int[] nums) 
+    {
+        // List<List<Integer>> ans = new ArrayList<>();
+        // ans.add(new ArrayList<>());
 
+        // for(int num : nums) 
+        // {
+        //     int size = ans.size();
+        //     for(int i = 0; i < size; i++) 
+        //     {
+        //         List<Integer> list = new ArrayList<>(ans.get(i));
+        //         list.add(num);
+        //         ans.add(list);
+        //     }
+        // }
+        // return ans;
         List<List<Integer>> ans = new ArrayList<>();
-        ans.add(new ArrayList<>());
-
-        for(int num : nums) 
-        {
-            int size = ans.size();
-            for(int i = 0; i < size; i++) 
-            {
-                List<Integer> list = new ArrayList<>(ans.get(i));
-                list.add(num);
-                ans.add(list);
-            }
-        }
+        solve(nums, 0, new ArrayList<>(), ans);
         return ans;
+    }
+    private void solve(int[] nums, int index,  List<Integer> list,List<List<Integer>> ans)
+    {
+        if(index == nums.length) 
+        {
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+        list.add(nums[index]);
+        solve(nums, index + 1, list, ans);
+        list.remove(list.size() - 1);
+        solve(nums, index + 1, list, ans);
     }
 }
