fb411da525b83498e9ef531e985cbb67b49d0dbc
Siddhesh Pokharkar <143425825+SiddheshPok@users.noreply.github.com>
2026-09-02 08:09:51 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (86.60%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -1,27 +1,47 @@
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        if (head == null || head.next == null) return null;
+    //     if (head == null || head.next == null) return null;
 
-        ListNode slow = head;
-        ListNode fast = head;
+    //     ListNode slow = head;
+    //     ListNode fast = head;
 
-       while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+    //    while (fast != null && fast.next != null) {
+    //         slow = slow.next;
+    //         fast = fast.next.next;
 
-            if (slow == fast) {
-                ListNode ptr1 = head;
-                ListNode ptr2 = slow;
+    //         if (slow == fast) {
+    //             ListNode ptr1 = head;
+    //             ListNode ptr2 = slow;
 
-                while (ptr1 != ptr2) {
-                    ptr1 = ptr1.next;
-                    ptr2 = ptr2.next;
-                }
+    //             while (ptr1 != ptr2) {
+    //                 ptr1 = ptr1.next;
+    //                 ptr2 = ptr2.next;
+    //             }
+    //             return ptr1;  
+    //         }
+    //     }
 
-                return ptr1;  
+    //     return null; 
+    if (head == null || head.next == null) return null;
+    ListNode slow = head;
+    ListNode fast = head;
+    while(fast != null && fast.next != null)
+    {
+        slow = slow.next;
+        fast = fast.next.next;
+
+        if(slow == fast)
+        {
+            ListNode ptr1 = head;
+            ListNode ptr2 = slow;
+            while(ptr1 != ptr2)
+            {
+                ptr1 = ptr1.next;
+                ptr2 = ptr2.next;
             }
+            return ptr1;
         }
-
-        return null; 
+    }
+    return null;
     }
 }
