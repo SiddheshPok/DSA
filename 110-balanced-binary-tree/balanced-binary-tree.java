64a23a760e156e85a7681039563ba85df8935829
Siddhesh Pokharkar <143425825+SiddheshPok@users.noreply.github.com>
2026-09-09 22:35:00 +0530

    Time: 3 ms (2.88%) | Memory: 46.2 MB (5.09%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -0,0 +1,67 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution 
+{
+    private int calculateHeight(TreeNode root)
+    {
+        List<List<Integer>> ans = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+
+        if( root == null ) return 0;
+        q.add(root);
+        while(!q.isEmpty())
+        {
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+            for( int i = 0; i < size; i++ )
+            {
+                TreeNode node = q.poll();
+                level.add(node.val);
+                if(node.left != null) 
+                {
+                    q.add(node.left);
+                }
+                if(node.right != null) 
+                {
+                    q.add(node.right);
+                }
+            }
+            ans.add(level);
+        }
+        return ans.size();
+    }
+    public boolean isBalanced(TreeNode root) 
+    {
+        if( root == null ) return true;   
+
+        int leftheight = calculateHeight(root.left);
+        int rightheight = calculateHeight(root.right);
+        int ans = Math.abs(leftheight - rightheight);
+        if( ans > 1 )
+        {
+            return false;
+        }
+        if(!isBalanced(root.left))
+        {
+            return false;
+        }
+        if(!isBalanced(root.right))
+        {
+            return false;
+        }
+        return true;
+    }
+}
