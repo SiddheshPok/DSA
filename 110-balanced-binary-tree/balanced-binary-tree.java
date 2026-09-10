582cdc235c1455a5ce6e2f1751945d88a80c6fb7
Siddhesh Pokharkar <143425825+SiddheshPok@users.noreply.github.com>
2026-09-10 23:14:10 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (10.63%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -15,53 +15,68 @@
  */
 class Solution 
 {
-    private int calculateHeight(TreeNode root)
-    {
-        List<List<Integer>> ans = new ArrayList<>();
-        Queue<TreeNode> q = new LinkedList<>();
+    // private int calculateHeight(TreeNode root)
+    // {
+    //     List<List<Integer>> ans = new ArrayList<>();
+    //     Queue<TreeNode> q = new LinkedList<>();
 
+    //     if( root == null ) return 0;
+    //     q.add(root);
+    //     while(!q.isEmpty())
+    //     {
+    //         int size = q.size();
+    //         List<Integer> level = new ArrayList<>();
+    //         for( int i = 0; i < size; i++ )
+    //         {
+    //             TreeNode node = q.poll();
+    //             level.add(node.val);
+    //             if(node.left != null) 
+    //             {
+    //                 q.add(node.left);
+    //             }
+    //             if(node.right != null) 
+    //             {
+    //                 q.add(node.right);
+    //             }
+    //         }
+    //         ans.add(level);
+    //     }
+    //     return ans.size();
+    // }
+    // public boolean isBalanced(TreeNode root) 
+    // {
+    //     if( root == null ) return true;   
+
+    //     int leftheight = calculateHeight(root.left);
+    //     int rightheight = calculateHeight(root.right);
+    //     int ans = Math.abs(leftheight - rightheight);
+    //     if( ans > 1 )
+    //     {
+    //         return false;
+    //     }
+    //     if(!isBalanced(root.left))
+    //     {
+    //         return false;
+    //     }
+    //     if(!isBalanced(root.right))
+    //     {
+    //         return false;
+    //     }
+    //     return true;
+    // }
+
+    private int calculateDepth(TreeNode root){
         if( root == null ) return 0;
-        q.add(root);
-        while(!q.isEmpty())
-        {
-            int size = q.size();
-            List<Integer> level = new ArrayList<>();
-            for( int i = 0; i < size; i++ )
-            {
-                TreeNode node = q.poll();
-                level.add(node.val);
-                if(node.left != null) 
-                {
-                    q.add(node.left);
-                }
-                if(node.right != null) 
-                {
-                    q.add(node.right);
-                }
-            }
-            ans.add(level);
-        }
-        return ans.size();
+        int left = calculateDepth(root.left);
+        int right = calculateDepth(root.right);
+        return 1  + Math.max(left, right);
     }
-    public boolean isBalanced(TreeNode root) 
-    {
-        if( root == null ) return true;   
+    public boolean isBalanced(TreeNode root) {
+        if( root == null ) return true;
+        int left = calculateDepth(root.left);
+        int right = calculateDepth(root.right);
 
-        int leftheight = calculateHeight(root.left);
-        int rightheight = calculateHeight(root.right);
-        int ans = Math.abs(leftheight - rightheight);
-        if( ans > 1 )
-        {
-            return false;
-        }
-        if(!isBalanced(root.left))
-        {
-            return false;
-        }
-        if(!isBalanced(root.right))
-        {
-            return false;
-        }
-        return true;
+        if (Math.abs(left - right) > 1) return false;
+        return isBalanced(root.left) && isBalanced(root.right);
     }
 }
