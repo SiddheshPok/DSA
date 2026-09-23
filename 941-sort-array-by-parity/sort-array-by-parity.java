748f0409cf8cc0bb5623eed023bf1e9e0a22f22d
Siddhesh Pokharkar <143425825+SiddheshPok@users.noreply.github.com>
2026-09-23 18:07:54 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (84.43%) - LeetSync

---
+++ 941-sort-array-by-parity/sort-array-by-parity.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] sortArrayByParity(int[] nums) {
+        int n = nums.length;
+        int ans[] = new int[n];
+        int l = 0;
+        int r = n - 1;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] % 2 == 0){
+                ans[l++] = nums[i];
+            }
+            else {
+                ans[r--] = nums[i];
+            }
+        }
+        return ans;
+    }
+}
