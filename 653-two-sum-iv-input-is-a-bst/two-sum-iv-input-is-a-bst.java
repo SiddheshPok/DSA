b184de90eac37af6061c9a72d1f8da5579533c86
Siddhesh Pokharkar <143425825+SiddheshPok@users.noreply.github.com>
2026-09-25 14:03:37 +0530

    Time: 22 ms (5.57%) | Memory: 46.8 MB (95.87%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -15,20 +15,30 @@
  */
 class Solution {
     List<Integer> list = new ArrayList<>();
-    boolean ans = false;
-    public boolean findTarget(TreeNode root, int k) {
-        dfs(root,k);
-        return ans;
-    }
+    // boolean ans = false;
+    // public boolean findTarget(TreeNode root, int k) {
+    //     dfs(root,k);
+    //     return ans;
+    // }
 
-    private void dfs(TreeNode root, int k) {
-        if(root == null) return;
-        if(list.contains(k - root.val)) { 
-            ans = true;
-        }
+    // private void dfs(TreeNode root, int k) {
+    //     if(root == null) return;
+    //     if(list.contains(k - root.val)) { 
+    //         ans = true;
+    //     }
         
+    //     list.add(root.val);
+    //     dfs(root.left, k);
+    //     dfs(root.right, k);
+    // }
+
+    public boolean findTarget(TreeNode root, int k) {
+        if(root == null) return false;
+
+        if(list.contains(k-root.val)) {
+            return true;
+        }
         list.add(root.val);
-        dfs(root.left, k);
-        dfs(root.right, k);
+        return findTarget(root.left, k) ||  findTarget(root.right, k);
     }
 }
